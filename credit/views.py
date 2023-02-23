cb297b684cf7d997a125df20af020a5112b76c75
Elif <elifintizam@gmail.com>
2023-02-23 23:01:46 +0300

    Organizing views, template features and 404 page

--- credit/views.py
+++ credit/views.py
@@ -2,38 +2,44 @@ from django.shortcuts import render
 
 from datetime import date
 
-credits = [
+all_credits = [
     {
         "slug":"mortgage",
         "image":"birinci.png",
         "date":date(2023, 2, 23),
         "title": "Idunno",
         "excerpt": "What is happening!",
-        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
+        "content":"111Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
     },
     {
-        "slug":"car loans",
-        "image":"birinci.png",
+        "slug":"car",
+        "image":"dorduncu.jpg",
         "date":date(2023, 5, 23),
         "title": "Idunno2",
         "excerpt": "What is happening!2",
-        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
+        "content":"222Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
     },
     {
-        "slug":"business lines of credit",
-        "image":"birinci.png",
+        "slug":"business",
+        "image":"ucuncu.png",
         "date":date(2023, 8, 23),
         "title": "Idunno3",
         "excerpt": "What is happening!3",
-        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
+        "content":"333Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos rem amet culpa harum repudiandae quae quam fugit soluta quo esse quos beatae neque, sapiente inventore. Saepe distinctio earum at." 
     }
 ]
 
+def get_date(credit):
+    return credit['date']
+
 def starting_page(request):
-    return render(request, "credit/index.html")
+    sorted_credits = sorted(all_credits, key=get_date)
+    latest_credits= sorted_credits[-3:]
+    return render(request, "credit/index.html", {"credits": latest_credits})
 
 def credits(request):
-    return render(request, "credit/all-credits.html")
+    return render(request, "credit/all-credits.html", {"all_credits":all_credits})
 
 def credit_detail(request,slug):
-    return render(request,"credit/credit-detail.html")
+    identified_credit= next(credit for credit in all_credits if credit['slug']== slug)
+    return render(request,"credit/credit-detail.html", {"credit":identified_credit})
