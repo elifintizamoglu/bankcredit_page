c3e037432d2d74ee5036fcc20fc68c42903afdcf
Elif <elifintizam@gmail.com>
2023-02-24 17:49:52 +0300

    Django Models and Migrations

--- credit/models.py
+++ credit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Credit(models.Model):
+    name = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=150)
+    content = models.CharField(max_length=500)
+    interest_rate = models.DecimalField(max_digits=6,decimal_places=4)
+    
